032918608e65e2fdea8facf31a67ac2751e10978
ashraf <32820483+ahmadashraf604@users.noreply.github.com>
2018-12-14 13:50:07 +0200

    after add insert,delete,select

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string.h>
-#include <string>
+#include <sstream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -32,8 +33,9 @@ public:
         head = tail = NULL;
     }
 
-    ~student(){
-    deleteALL();
+    ~student()
+    {
+        deleteALL();
     }
 
     // Append function to add new student
@@ -68,17 +70,17 @@ public:
     {
         studentStruct * temp = head;
         int counter =0;
-        cout << "No.  |  " << "ID" << "  |  " << "First Name" << "  |  " << "Last Name" << "  |  " << "Age" << "  |  " << "Department ID" << endl;
+        cout << "\tNo.\t|\t" << "ID" << "\t|\t"<< "First Name" << "\t|\t"<< "Last Name" <<"\t|\t"<< "Age" << "\t|\t" << "Department ID" << endl;
         while (temp != NULL)
         {
-            cout << counter++ << "  |  " << temp -> id << "  |  " << temp -> fName << "  |  " << temp -> lName << "  |  " << temp -> age << "  |  " << temp -> depID << endl;
+            cout <<"\t" << counter++ << "\t|\t" << temp -> id << "\t|\t"<< temp -> fName << "\t\t|\t"<< temp -> lName << "\t\t|\t" << temp -> age << "\t|\t" << temp -> depID << endl;
             temp = temp -> next;
         }
     }
 
     // Search
     // Search Student by ID
-    studentStruct * searchByID( string _id )
+    studentStruct * searchByID( string _id)
     {
         studentStruct * temp = head;
         while (temp != NULL)
@@ -99,71 +101,85 @@ public:
         while (temp != NULL)
         {
             if( _fName == temp -> fName )
-                {
-                    return temp;
-                }
+            {
+                return temp;
+            }
             temp = temp -> next;
         }
         return NULL;
     }
 
-    void deleteByID(string _id)
+    void deleteByID(string _id,string operation)
     {
-        studentStruct * temp = searchByID( _id );
-        if ( temp == NULL )
-            return;
-        if ( head == tail )
+        if(operation == "=")
         {
-            delete temp;
-            head = tail = NULL;
+            studentStruct * temp = searchByID( _id);
+            if ( temp == NULL )
+                return;
+            if ( head == tail )
+            {
+                delete temp;
+                head = tail = NULL;
+            }
+            else if ( temp == head )
+            {
+                head = temp -> next;
+                head -> prev = NULL;
+                delete temp;
+            }
+            else if ( temp == tail)
+            {
+                tail = temp -> prev;
+                tail -> next = NULL;
+                delete temp;
+            }
+            else
+            {
+                temp -> next -> prev = temp -> prev;
+                temp -> prev -> next = temp -> next;
+                delete temp;
+            }
         }
-        else if ( temp == head )
-        {
-            head = temp -> next;
-            head -> prev = NULL;
-            delete temp;
-        }
-        else if ( temp == tail){
-            tail = temp -> prev;
-            tail -> next = NULL;
-            delete temp;
-        } else {
-        temp -> next -> prev = temp -> prev;
-        temp -> prev -> next = temp -> next;
-        delete temp;
-        }
+
     }
 
 
     // Insert any student to the table after the current student ID
-    void insertByID_after(string _id, string _fName, string _lName, int _age, int _depID, string oldID){
-    studentStruct * temp = searchByID( oldID );
-    if ( temp == NULL || temp == tail ){
-        append( _id, _fName, _lName, _age, _depID );
-    } else {
-    studentStruct * newS = new studentStruct;
-    // Data
-    temp -> id = _id;
-    temp -> fName = _fName;
-    temp -> lName = _lName;
-    temp -> age = _age;
-    temp -> depID = _depID;
+    void insertByID_after(string _id, string _fName, string _lName, int _age, int _depID, string oldID)
+    {
+        studentStruct * temp = searchByID( oldID );
+        if ( temp == NULL || temp == tail )
+        {
+            append( _id, _fName, _lName, _age, _depID );
+        }
+        else
+        {
+            studentStruct * newS = new studentStruct;
+            // Data
+            temp -> id = _id;
+            temp -> fName = _fName;
+            temp -> lName = _lName;
+            temp -> age = _age;
+            temp -> depID = _depID;
 
-    newS -> prev = temp;
-    newS -> next = temp -> next;
-    temp -> next = newS;
-    newS -> next -> prev = newS;
-    }
+            newS -> prev = temp;
+            newS -> next = temp -> next;
+            temp -> next = newS;
+            newS -> next -> prev = newS;
+        }
     }
 
-    void deleteALL(){
-    studentStruct * temp = head;
-    while ( temp != NULL){
-        head = head -> next;
-        delete temp;
-        temp = head;
+    void deleteALL()
+    {
+        studentStruct * temp = head;
+        while ( temp != NULL)
+        {
+            head = head -> next;
+            delete temp;
+            temp = head;
+        }
     }
-    }
+
 };
 
 class department
@@ -176,8 +192,9 @@ public:
         head = tail = NULL;
     }
 
-    ~department(){
-    //deleteALL();
+    ~department()
+    {
+        //deleteALL();
     }
 
     // Append function to add new student
@@ -219,10 +236,60 @@ public:
         cout << "Department is not exist!" << endl;
         return NULL;
     }
+    //search department by id and return boolean
+    int searchDepartmentByID( int _depID )
+    {
+        departmentStruct * temp = head;
+        while (temp != NULL)
+        {
+            if( _depID == temp -> depID )
+            {
+                return 1;
+            }
+            temp = temp -> next;
+        }
+        return 0;
+    }
 };
+department d1;
+student s1;
 
+string seperat(string & inputData,string delemeter);
+void insertInto(string & inputData);
+void selectFrom(string & inputData);
+void deleteFrom(string & inputData);
 int main()
 {
+    do
+    {
+
+        string inputData;
+        getline (std::cin,inputData);
+        string operation = seperat(inputData," ");
+        if(operation == "help")
+        {
+            cout<<"to insert data in table : insert into tableName id,fname,lname,age,deptId"<<endl;
+            cout<<"to delete all data in table : delete from tableName"<<endl;
+            cout<<"to delete all data in table : delete from tableName where columnName = value"<<endl;
+            cout<<"to select all data in table : select * from tableName"<<endl;
+        }
+        else if(operation == "insert")
+        {
+            insertInto(inputData);
+        }
+        else if(operation == "select")
+        {
+            selectFrom(inputData);
+        }
+        else if(operation == "delete")
+        {
+            deleteFrom(inputData);
+        }
+        else
+            cout<<"invalid command";
+    }
+    while(1);
+    /*
     department d1;
     d1.append(956, "Java");
 
@@ -231,7 +298,7 @@ int main()
     s1.append("16512", "Ahmed", "Ashraf", 132, 956);
     s1.append("asdas16", "Esraa", "Fathy", 485, 956);
     s1.display();
-/*
+
     student s1;
     s1.append(1,"Ahmed");
     s1.append(2,"Mohamed");
@@ -251,7 +318,143 @@ int main()
     s1.deleteByName("Mohamed");
     s1.display();
 
-*/
+    */
 
     return 0;
+}
+
+string seperat(string & inputData,string delemeter)
+{
+    string operation=inputData.substr(0,inputData.find(delemeter));
+    inputData=inputData.substr(inputData.find(delemeter)+1);
+    if(inputData == operation)
+    {
+        inputData = "";
+        return operation;
+    }
+    return operation;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//insert into student or dept using command line
+void insertInto(string & inputData)
+{
+    string operation =seperat(inputData," ");
+    if(operation == "into")
+    {
+        operation =seperat(inputData," ");
+        if(operation == "student")
+        {
+            string id =seperat(inputData,",");
+            string fname =seperat(inputData,",");
+            string lname =seperat(inputData,",");
+            string age =seperat(inputData,",");
+            string deptid =seperat(inputData,",");
+            if(id != "" && fname != "" && lname != "" && age != "" && deptid != "")
+            {
+                if(d1.searchDepartmentByID(atoi(deptid.c_str())))
+                    s1.append(id,fname, lname, atoi(age.c_str()), atoi(deptid.c_str()));
+                else
+                    cout<<"invalid department id";
+            }
+            else
+                cout<<"invalid command";
+        }
+        else if(operation == "dept")
+        {
+            string id =seperat(inputData,",");
+            string name =seperat(inputData,",");
+            if(id != "" && name != "")
+                d1.append(atoi(id.c_str()), name);
+            else
+                cout<<"invalid command";
+        }
+        else
+            cout<<"invalid table name";
+    }
+    else
+        cout<<"invalid command";
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//select from student or dept using command line
+void selectFrom(string & inputData)
+{
+    string operation =seperat(inputData," ");
+    if(operation == "*")
+    {
+        operation =seperat(inputData," ");
+        if(operation == "from")
+        {
+            operation =seperat(inputData," ");
+            if(operation == "student")
+            {
+                operation =seperat(inputData," ");
+                if(operation == "where")
+                {
+                    string columnName =seperat(inputData," ");
+                    if(columnName == "id" ||columnName == "name" ||columnName == "fname"
+                            ||columnName == "lname" ||columnName == "deptId" ||columnName == "age")
+                    {
+                        operation =seperat(inputData," ");
+                        if(operation == "=" ||operation == "=<" ||operation == "=>" ||operation == "<" ||
+                                operation == ">" ||operation == "!=")
+                        {
+                            string value = seperat(inputData," ");
+                            if(value != "")
+                            {
+                                //here we will call function that take (columnName ,operation,value)
+                            }
+                            else
+                                cout<<"invalid command";
+                        }
+                        else
+                            cout<<"invalid command";
+                    }
+                    else
+                        cout<<"invalid command";
+                }
+                else
+                    s1.display();
+            }
+        }
+    }
+}
+void deleteFrom(string & inputData)
+{
+    string operation = seperat(inputData," ");
+    if(operation == "from")
+    {
+        operation = seperat(inputData," ");
+        if(operation == "student")
+        {
+            operation = seperat(inputData," ");
+            if(operation == "where")
+            {
+                string columnName =seperat(inputData," ");
+                if(columnName == "id" ||columnName == "name" ||columnName == "fname"
+                        ||columnName == "lname" ||columnName == "deptId" ||columnName == "age")
+                {
+                    operation =seperat(inputData," ");
+                    if(operation == "=" ||operation == "=<" ||operation == "=>" ||operation == "<" ||
+                            operation == ">" ||operation == "!=")
+                    {
+                        string value = seperat(inputData," ");
+                        if(value != "")
+                        {
+                            s1.deleteByID(value, operation);
+                            //here we will call function that take (columnName ,operation,value)
+                        }
+                        else
+                            cout<<"invalid command";
+                    }
+                    else
+                        cout<<"invalid command";
+                }
+                else
+                    cout<<"invalid command";
+            }
+            else
+                s1.deleteALL();
+        }
+    }
 }
